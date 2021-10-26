b8db442ea3de414772616851eae4e371b14ea8d3
DYZparker <qq182345999@163.com>
2021-10-26 23:05:50 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+  const handlelogin = () => {
+    console.log()
+  }
+</script>
+
+<template>
+  <div class="header">
+    <span>云中博客后台管理系统</span>
+    <el-button
+      class="log-button"
+      size="mini"
+      type="danger"
+      @click="handlelogin">退出</el-button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  span {
+    color: #fff;
+    font-size: 30px;
+    margin-left: 230px;
+  }
+  .log-button {
+    font-size: 15px;
+    margin: 5px 100px;
+  }
+}
+</style>
